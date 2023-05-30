d1b32f788fe79ee92e5eba30520c072ca1c9050c
baota <baota@efe.com.vn>
2023-05-30 17:23:58 +0700

    update

---
+++ assets/js/myjs.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(() =>{
+   jQuery(".slider-before-and-after").slick({
+        slidesToShow: 2,
+        slidesToScroll: 2, 
+        autoplay: true,
+        autoplaySpeed: 2000,
+    })
+})
