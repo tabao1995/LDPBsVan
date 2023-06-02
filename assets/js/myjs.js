a1c78ceacc17e15c1df44a17fad57e7ea1f02be3
baota <baota@efe.com.vn>
2023-06-02 23:51:55 +0700

    update

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -4,6 +4,22 @@ jQuery(document).ready(() =>{
         slidesToScroll: 4, 
         autoplay: true,
         autoplaySpeed: 2000,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+              }
+            },  {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2,
+                }
+              },
+        ]
+        
     })
     jQuery(".custom-review").slick({
         slidesToShow: 2 ,
@@ -11,5 +27,13 @@ jQuery(document).ready(() =>{
         arrows:false,
         autoplay: true,
         autoplaySpeed: 3000,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+              }
+            }]
     })
 })
