a38eadab736676c0af23e13335a5c4748f48270a
baota <baota@efe.com.vn>
2023-06-27 00:18:05 +0700

    update

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -1,39 +1,42 @@
-jQuery(document).ready(() =>{
-   jQuery(".slider-before-and-after").slick({
-        slidesToShow: 4,
-        slidesToScroll: 4, 
-        autoplay: true,
-        autoplaySpeed: 2000,
-        responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-              }
-            },  {
-                breakpoint: 768,
-                settings: {
-                  slidesToShow: 2,
-                  slidesToScroll: 2,
-                }
-              },
-        ]
-        
-    })
-    jQuery(".custom-review").slick({
-        slidesToShow: 2 ,
-        slidesToScroll: 1, 
-        arrows:false,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-              }
-            }]
-    })
-})
+jQuery(document).ready(() => {
+  jQuery(".slider-before-and-after").slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+    ],
+  });
+  jQuery(".custom-review").slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+  // let height = jQuery(window).width() * (831 / 1921);
+  // jQuery(".banner").attr("style", "height:" + height + "px");
+});
