48be6e42858bc976d38bddf06bd09d77eef980a6
baota <baota@efe.com.vn>
2023-08-07 13:02:54 +0700

    update

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -39,4 +39,24 @@ jQuery(document).ready(() => {
   });
   // let height = jQuery(window).width() * (831 / 1921);
   // jQuery(".banner").attr("style", "height:" + height + "px");
+  jQuery(".section-8-new__slider").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
 });
