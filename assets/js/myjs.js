ba9a96ee4082b8ca3bda69d5a151bcc696c870a6
baota <baota@efe.com.vn>
2023-09-20 23:21:47 +0700

    update

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -78,4 +78,29 @@ jQuery(document).ready(() => {
     autoplaySpeed: 1000,
     dots: false,
   });
+  var startDate = new Date();
+  var endDate = new Date();
+  endDate.setDate(startDate.getDate() + 1);
+
+  function updateCountdown() {
+    var now = new Date();
+    var timeLeft = endDate - now;
+
+    if (timeLeft <= 0) {
+      $(".countdown-number").text("0");
+    } else {
+      var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+      var hours = Math.floor(
+        (timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+      $("#days").text(days);
+      $("#hours").text(hours);
+      $("#minutes").text(minutes);
+      $("#seconds").text(seconds);
+    }
+  }
+  setInterval(updateCountdown, 1000);
 });
